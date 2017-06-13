404fd2c0e7308f48bb34f3467c2e8e96ea52d0ab
Dan Moran <dan.moran@gmail.com>
2017-06-13 17:48:19 +0200

    fix for nonsquare images

--- aframe-heatmap3d.js
+++ aframe-heatmap3d.js
@@ -205,15 +205,6 @@
 
 
 
-    /*
-    if ("opacityMin" in diff && data.scaleOpacity && data.scaleOpacityMethod==="const" && this.el.getObject3D('terrain-heatmap')){
-      this.el.getObject3D('terrain-heatmap').material.opacity = data.opacityMin;
-    }
-
-    if ("wireframe" in diff && this.el.getObject3D('terrain-heatmap')){
-      this.el.getObject3D('terrain-heatmap').material.wireframe = data.wireframe;
-    }
-  */
 
 
 
@@ -237,7 +228,8 @@
       } else if ("aquablues" === data.palette) {
         this.palette = ['#f7fcf0','#e0f3db','#ccebc5','#a8ddb5','#7bccc4','#4eb3d3','#2b8cbe','#08589e'];
       } else if ("reds" === data.palette) {
-        this.palette = ['#fff7ec','#fee8c8','#fdd49e','#fdbb84','#fc8d59','#ef6548','#d7301f','#990000'];
+        this.palette = ['#E5E5E5','#E6DEDE','#E7D7D7','#E7D0D0','#E8C9C9','#E9C2C2','#EABBBB','#EAB4B4','#EBADAD','#ECA6A6','#ED9F9F','#EE9898','#EE9292','#EF8B8B','#F08484','#F17D7D','#F17676','#F26F6F','#F36868','#F46161','#F45A5A','#F55353','#F64C4C','#F74545','#F83E3E','#F83737','#F93030','#FA2929','#FB2222','#FB1B1B','#FC1414','#FD0D0D','#FE0606','#FF0000','#FB0001','#F80103','#F50204','#F20206','#EF0307','#EC0409','#E9040A','#E6050C','#E3060D','#E0060F','#DD0710','#DA0812','#D70813','#D40915','#D10A16','#CE0A18','#CA0B1A','#C70C1B','#C40C1D','#C10D1E','#BE0E20','#BB0E21','#B80F23','#B51024','#B21026','#AF1127','#AC1229','#A9132A','#A6132C','#A3142D'];
+        //this.palette = ['#fff7ec','#fee8c8','#fdd49e','#fdbb84','#fc8d59','#ef6548','#d7301f','#990000'];
       } else if ("redblue" === data.palette) {
         this.palette = ["#2166ac", "#4393c3", "#92c5de", "#d1e5f0", "#fddbc7", "#f4a582", "#d6604d", "#b2182b"];  
       } else if ("RdYlBu" === data.palette) {
@@ -252,6 +244,8 @@
         this.palette =['#0C0786','#100787','#130689','#15068A','#18068B','#1B068C','#1D068D','#1F058E','#21058F','#230590','#250591','#270592','#290593','#2B0594','#2D0494','#2F0495','#310496','#330497','#340498','#360498','#380499','#3A049A','#3B039A','#3D039B','#3F039C','#40039C','#42039D','#44039E','#45039E','#47029F','#49029F','#4A02A0','#4C02A1','#4E02A1','#4F02A2','#5101A2','#5201A3','#5401A3','#5601A3','#5701A4','#5901A4','#5A00A5','#5C00A5','#5E00A5','#5F00A6','#6100A6','#6200A6','#6400A7','#6500A7','#6700A7','#6800A7','#6A00A7','#6C00A8','#6D00A8','#6F00A8','#7000A8','#7200A8','#7300A8','#7500A8','#7601A8','#7801A8','#7901A8','#7B02A8','#7C02A7','#7E03A7','#7F03A7','#8104A7','#8204A7','#8405A6','#8506A6','#8607A6','#8807A5','#8908A5','#8B09A4','#8C0AA4','#8E0CA4','#8F0DA3','#900EA3','#920FA2','#9310A1','#9511A1','#9612A0','#9713A0','#99149F','#9A159E','#9B179E','#9D189D','#9E199C','#9F1A9B','#A01B9B','#A21C9A','#A31D99','#A41E98','#A51F97','#A72197','#A82296','#A92395','#AA2494','#AC2593','#AD2692','#AE2791','#AF2890','#B02A8F','#B12B8F','#B22C8E','#B42D8D','#B52E8C','#B62F8B','#B7308A','#B83289','#B93388','#BA3487','#BB3586','#BC3685','#BD3784','#BE3883','#BF3982','#C03B81','#C13C80','#C23D80','#C33E7F','#C43F7E','#C5407D','#C6417C','#C7427B','#C8447A','#C94579','#CA4678','#CB4777','#CC4876','#CD4975','#CE4A75','#CF4B74','#D04D73','#D14E72','#D14F71','#D25070','#D3516F','#D4526E','#D5536D','#D6556D','#D7566C','#D7576B','#D8586A','#D95969','#DA5A68','#DB5B67','#DC5D66','#DC5E66','#DD5F65','#DE6064','#DF6163','#DF6262','#E06461','#E16560','#E26660','#E3675F','#E3685E','#E46A5D','#E56B5C','#E56C5B','#E66D5A','#E76E5A','#E87059','#E87158','#E97257','#EA7356','#EA7455','#EB7654','#EC7754','#EC7853','#ED7952','#ED7B51','#EE7C50','#EF7D4F','#EF7E4E','#F0804D','#F0814D','#F1824C','#F2844B','#F2854A','#F38649','#F38748','#F48947','#F48A47','#F58B46','#F58D45','#F68E44','#F68F43','#F69142','#F79241','#F79341','#F89540','#F8963F','#F8983E','#F9993D','#F99A3C','#FA9C3B','#FA9D3A','#FA9F3A','#FAA039','#FBA238','#FBA337','#FBA436','#FCA635','#FCA735','#FCA934','#FCAA33','#FCAC32','#FCAD31','#FDAF31','#FDB030','#FDB22F','#FDB32E','#FDB52D','#FDB62D','#FDB82C','#FDB92B','#FDBB2B','#FDBC2A','#FDBE29','#FDC029','#FDC128','#FDC328','#FDC427','#FDC626','#FCC726','#FCC926','#FCCB25','#FCCC25','#FCCE25','#FBD024','#FBD124','#FBD324','#FAD524','#FAD624','#FAD824','#F9D924','#F9DB24','#F8DD24','#F8DF24','#F7E024','#F7E225','#F6E425','#F6E525','#F5E726','#F5E926','#F4EA26','#F3EC26','#F3EE26','#F2F026','#F2F126','#F1F326','#F0F525','#F0F623','#EFF821'];
       } else if ("viridis" === data.palette) {
         this.palette = ['#440154','#440255','#440357','#450558','#45065A','#45085B','#46095C','#460B5E','#460C5F','#460E61','#470F62','#471163','#471265','#471466','#471567','#471669','#47186A','#48196B','#481A6C','#481C6E','#481D6F','#481E70','#482071','#482172','#482273','#482374','#472575','#472676','#472777','#472878','#472A79','#472B7A','#472C7B','#462D7C','#462F7C','#46307D','#46317E','#45327F','#45347F','#453580','#453681','#443781','#443982','#433A83','#433B83','#433C84','#423D84','#423E85','#424085','#414186','#414286','#404387','#404487','#3F4587','#3F4788','#3E4888','#3E4989','#3D4A89','#3D4B89','#3D4C89','#3C4D8A','#3C4E8A','#3B508A','#3B518A','#3A528B','#3A538B','#39548B','#39558B','#38568B','#38578C','#37588C','#37598C','#365A8C','#365B8C','#355C8C','#355D8C','#345E8D','#345F8D','#33608D','#33618D','#32628D','#32638D','#31648D','#31658D','#31668D','#30678D','#30688D','#2F698D','#2F6A8D','#2E6B8E','#2E6C8E','#2E6D8E','#2D6E8E','#2D6F8E','#2C708E','#2C718E','#2C728E','#2B738E','#2B748E','#2A758E','#2A768E','#2A778E','#29788E','#29798E','#287A8E','#287A8E','#287B8E','#277C8E','#277D8E','#277E8E','#267F8E','#26808E','#26818E','#25828E','#25838D','#24848D','#24858D','#24868D','#23878D','#23888D','#23898D','#22898D','#228A8D','#228B8D','#218C8D','#218D8C','#218E8C','#208F8C','#20908C','#20918C','#1F928C','#1F938B','#1F948B','#1F958B','#1F968B','#1E978A','#1E988A','#1E998A','#1E998A','#1E9A89','#1E9B89','#1E9C89','#1E9D88','#1E9E88','#1E9F88','#1EA087','#1FA187','#1FA286','#1FA386','#20A485','#20A585','#21A685','#21A784','#22A784','#23A883','#23A982','#24AA82','#25AB81','#26AC81','#27AD80','#28AE7F','#29AF7F','#2AB07E','#2BB17D','#2CB17D','#2EB27C','#2FB37B','#30B47A','#32B57A','#33B679','#35B778','#36B877','#38B976','#39B976','#3BBA75','#3DBB74','#3EBC73','#40BD72','#42BE71','#44BE70','#45BF6F','#47C06E','#49C16D','#4BC26C','#4DC26B','#4FC369','#51C468','#53C567','#55C666','#57C665','#59C764','#5BC862','#5EC961','#60C960','#62CA5F','#64CB5D','#67CC5C','#69CC5B','#6BCD59','#6DCE58','#70CE56','#72CF55','#74D054','#77D052','#79D151','#7CD24F','#7ED24E','#81D34C','#83D34B','#86D449','#88D547','#8BD546','#8DD644','#90D643','#92D741','#95D73F','#97D83E','#9AD83C','#9DD93A','#9FD938','#A2DA37','#A5DA35','#A7DB33','#AADB32','#ADDC30','#AFDC2E','#B2DD2C','#B5DD2B','#B7DD29','#BADE27','#BDDE26','#BFDF24','#C2DF22','#C5DF21','#C7E01F','#CAE01E','#CDE01D','#CFE11C','#D2E11B','#D4E11A','#D7E219','#DAE218','#DCE218','#DFE318','#E1E318','#E4E318','#E7E419','#E9E419','#ECE41A','#EEE51B','#F1E51C','#F3E51E','#F6E61F','#F8E621','#FAE622','#FDE724'];
+      } else if ("inferno" === data.palette) {
+        this.palette = ['#000003','#000004','#000006','#010007','#010109','#01010B','#02010E','#020210','#030212','#040314','#040316','#050418','#06041B','#07051D','#08061F','#090621','#0A0723','#0B0726','#0D0828','#0E082A','#0F092D','#10092F','#120A32','#130A34','#140B36','#160B39','#170B3B','#190B3E','#1A0B40','#1C0C43','#1D0C45','#1F0C47','#200C4A','#220B4C','#240B4E','#260B50','#270B52','#290B54','#2B0A56','#2D0A58','#2E0A5A','#300A5C','#32095D','#34095F','#350960','#370961','#390962','#3B0964','#3C0965','#3E0966','#400966','#410967','#430A68','#450A69','#460A69','#480B6A','#4A0B6A','#4B0C6B','#4D0C6B','#4F0D6C','#500D6C','#520E6C','#530E6D','#550F6D','#570F6D','#58106D','#5A116D','#5B116E','#5D126E','#5F126E','#60136E','#62146E','#63146E','#65156E','#66156E','#68166E','#6A176E','#6B176E','#6D186E','#6E186E','#70196E','#72196D','#731A6D','#751B6D','#761B6D','#781C6D','#7A1C6D','#7B1D6C','#7D1D6C','#7E1E6C','#801F6B','#811F6B','#83206B','#85206A','#86216A','#88216A','#892269','#8B2269','#8D2369','#8E2468','#902468','#912567','#932567','#952666','#962666','#982765','#992864','#9B2864','#9C2963','#9E2963','#A02A62','#A12B61','#A32B61','#A42C60','#A62C5F','#A72D5F','#A92E5E','#AB2E5D','#AC2F5C','#AE305B','#AF315B','#B1315A','#B23259','#B43358','#B53357','#B73456','#B83556','#BA3655','#BB3754','#BD3753','#BE3852','#BF3951','#C13A50','#C23B4F','#C43C4E','#C53D4D','#C73E4C','#C83E4B','#C93F4A','#CB4049','#CC4148','#CD4247','#CF4446','#D04544','#D14643','#D24742','#D44841','#D54940','#D64A3F','#D74B3E','#D94D3D','#DA4E3B','#DB4F3A','#DC5039','#DD5238','#DE5337','#DF5436','#E05634','#E25733','#E35832','#E45A31','#E55B30','#E65C2E','#E65E2D','#E75F2C','#E8612B','#E9622A','#EA6428','#EB6527','#EC6726','#ED6825','#ED6A23','#EE6C22','#EF6D21','#F06F1F','#F0701E','#F1721D','#F2741C','#F2751A','#F37719','#F37918','#F47A16','#F57C15','#F57E14','#F68012','#F68111','#F78310','#F7850E','#F8870D','#F8880C','#F88A0B','#F98C09','#F98E08','#F99008','#FA9107','#FA9306','#FA9506','#FA9706','#FB9906','#FB9B06','#FB9D06','#FB9E07','#FBA007','#FBA208','#FBA40A','#FBA60B','#FBA80D','#FBAA0E','#FBAC10','#FBAE12','#FBB014','#FBB116','#FBB318','#FBB51A','#FBB71C','#FBB91E','#FABB21','#FABD23','#FABF25','#FAC128','#F9C32A','#F9C52C','#F9C72F','#F8C931','#F8CB34','#F8CD37','#F7CF3A','#F7D13C','#F6D33F','#F6D542','#F5D745','#F5D948','#F4DB4B','#F4DC4F','#F3DE52','#F3E056','#F3E259','#F2E45D','#F2E660','#F1E864','#F1E968','#F1EB6C','#F1ED70','#F1EE74','#F1F079','#F1F27D','#F2F381','#F2F485','#F3F689','#F4F78D','#F5F891','#F6FA95','#F7FB99','#F9FC9D','#FAFDA0','#FCFEA4'];
       } else if ("parula" === data.palette) {
         this.palette = ['#3D26A8','#3F2AB4','#412EBF','#4332CA','#4536D5','#463BDE','#4641E5','#4746EB','#474CF0','#4752F4','#4757F7','#465DFA','#4463FC','#4269FD','#3E6FFE','#3875FE','#327BFC','#2E81F9','#2D86F6','#2C8CF2','#2B91EE','#2796EB','#259BE7','#23A0E4','#1FA4E2','#1CA9DF','#18ADDB','#11B1D6','#07B4D0','#00B7C9','#01BAC3','#0BBCBC','#18BFB5','#23C1AE','#2BC3A7','#31C59F','#37C797','#3EC98D','#4ACB84','#56CC7A','#63CC6F','#71CC63','#80CB58','#8FCA4D','#9DC842','#ABC638','#B8C430','#C5C129','#D1BF27','#DCBC28','#E6BA2D','#EFB935','#F8BA3D','#FDBD3C','#FEC338','#FDC933','#FCCF30','#F9D52D','#F6DC29','#F5E227','#F4E824','#F5EF20','#F7F41B','#F9FA14'];
       } else if ("hot" === data.palette) {
@@ -285,7 +279,7 @@
 
           // We'll read the Red value from the image data to get height value for each pixel
           this.imgBytes = this.canvasContext.getImageData(0, 0,this.canvas.width, this.canvas.height).data;
-          var heights = new Float32Array(this.imgBytes.length/4);
+          this.heights = new Float32Array(this.imgBytes.length/4);
 
 
           // Do a pass over the data to get max and min values
@@ -306,13 +300,13 @@
           // Extract heights from image data
           // In the image white=255 RGB but equals 0 elevation, and black=0=1 elevation
           for (ci=0, di=0; ci<this.imgBytes.length; ci+=4) {
-              heights[di++] = this.imgBytes[ci];
+              this.heights[di++] = this.imgBytes[ci];
           }
-          for (ci=0; ci<heights.length; ci++ ){
-            heights[ci] = 1- (heights[ci]-this.minPixelVal) /(this.maxPixelVal-this.minPixelVal);
+          for (ci=0; ci<this.heights.length; ci++ ){
+            this.heights[ci] = 1- (this.heights[ci]-this.minPixelVal) /(this.maxPixelVal-this.minPixelVal);
             //if (data.invertElevation) heights[ci]=1-heights[ci];
             //if (data.stackBlurRadius>0 && heights[ci]==1/255) heights[ci] = 0;
-            if (data.ignoreTransparentValues && this.imgBytes[ci*4 + 3]===0 ) heights[ci]=-1;
+            if (data.ignoreTransparentValues && this.imgBytes[ci*4 + 3]===0 ) this.heights[ci]=-1;
           }
 
 
@@ -321,7 +315,7 @@
            */
           console.time("aframe-heatmap3d: base geometry");
           //var geometry = new THREE.PlaneBufferGeometry(data.width, data.height, data.canvas.width-1, data.canvas.height-1);
-          this.geometry = new Heatmap3dPlaneBufferGeometry(data.width, data.height, this.canvas.width-1, this.canvas.height-1, heights, data.ignoreZeroValues, data.ignoreTransparentValues);
+          this.geometry = new Heatmap3dPlaneBufferGeometry(data.width, data.height, this.canvas.width-1, this.canvas.height-1, this.heights, data.ignoreZeroValues, data.ignoreTransparentValues);
           console.timeEnd("aframe-heatmap3d: base geometry");
 
 
@@ -347,7 +341,7 @@
     if (data.scaleOpacityMethod==="const") sm=3;
 
 
-    data.updateMaterial = (this.material===undefined || "emissive" in diff || "roughness" in diff || "shininess" in diff || "emissiveIntensity" in diff || "opacityMin" in diff || "opacityMax" in diff || "palette" in diff || "scaleOpacityMethod" in diff || "wireframe" in diff || "material" in diff  || "renderMode" in diff  || "particleSize" in diff  );
+    data.updateMaterial = (this.material===undefined || "emissive" in diff || "flipPalette" in diff || "roughness" in diff  || "metalness" in diff  || "shininess" in diff || "emissiveIntensity" in diff || "opacityMin" in diff || "opacityMax" in diff || "palette" in diff || "scaleOpacityMethod" in diff ||"scaleOpacity" in diff || "wireframe" in diff || "material" in diff  || "renderMode" in diff  || "particleSize" in diff  );
 
     if (data.updateMaterial ) {
         console.time("aframe-heatmap3d: update material");
@@ -360,7 +354,7 @@
 
         for (vi=2, di=0, ci=0;    vi<NVERTS*3;   vi+=3, di++, ci+=4) {
           // Get this pixels' elevation, in the range 0-1. Do 1- so that white=0 and black=1
-          val = Math.max(0, heights[di]);
+          val = Math.max(0, this.heights[di]);
 
           // Calculate opacity
           if (sm===-1) {
@@ -398,7 +392,7 @@
          * See: https://github.com/mrdoob/three.js/issues/2118
          * If there is no transparency, we can use a normal Lambert material.
          */
-        if (data.scaleOpacity && data.scaleOpacityMethod!=="const" && data.material=="default") {
+        if (data.scaleOpacity && data.scaleOpacityMethod!=="const" && data.material=="default" && !data.wireframe) {
           this.material = new THREE.ShaderMaterial({
             vertexShader:   this.customVertexShader,
             fragmentShader: this.customFragShader,
